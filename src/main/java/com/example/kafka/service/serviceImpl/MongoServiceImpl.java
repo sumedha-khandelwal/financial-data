83fa17284777d19a098a7759f5aec566c29316b2
Sumedha.Khandelwal <sumedha.khandelwal@timesinternet.in>
2019-06-19 16:14:19 +0530

    update

---
+++ src/main/java/com/example/kafka/service/serviceImpl/MongoServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.kafka.service.serviceImpl;
+
+public class MongoServiceImpl {
+}
---
+++ src/test/java/com/example/kafka/Controller/ProducerControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.kafka.Controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProducerControllerTest {
+
+    @Test
+    public void startProducer() {
+    }
+
+    @Test
+    public void stopProducer() {
+    }
+
+    @Test
+    public void interruptProducer() {
+    }
+}
